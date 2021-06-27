387868f910e76995881c67ea78ee51d170e3badc
oluwablin <oyelekeseun@outlook.com>
2021-06-27 16:23:08 +0100

    Project Setup, Schema and relationships

---
+++ README.md
@@ -0,0 +1,50 @@
+
+## About Application
+
+A simple CRM application for a Law Firm
+
+## Project Setup
+
+### Cloning the GitHub Repository
+
+Clone the repository to your local machine by running the terminal command below.
+
+```bash
+git clone https://github.com/Oluwablin/law-crm
+```
+
+### Setup Database
+
+Create a MySQL database and note down the required connection parameters. (DB Host, Username, Password, Name)
+
+### Install Composer Dependencies
+
+Navigate to the project root directory via terminal and run the following command.
+
+```bash
+composer install
+```
+
+### Create a copy of your .env file
+
+Run the following command
+
+```bash
+cp .env.example .env
+```
+
+This should create an exact copy of the .env.example file. Name the newly created file .env and update it with your local environment variables (database connection info and others).
+
+### Generate an app encryption key
+
+```bash
+php artisan key:generate
+```
+
+### Migrate the database
+
+```bash
+php artisan migrate
+```
+
+
---
+++ database/migrations/2021_06_27_150434_create_clients_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email')->unique();
+            $table->date('date_profiled');
+            $table->string('primary_legal_counsel');
+            $table->date('date_of_birth');
+            $table->string('case_details');
+            $table->string('profile_image')->nullable();
+            $table->string('created_by');
+            $table->string('updated_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
